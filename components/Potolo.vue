7583c65e92734d45e209540f06813704ac20040c
leoratatata <143421236+leoratatata@users.noreply.github.com>
2024-11-08 12:53:22 +0000

    se creo el ciclo de vida de potolo

---
+++ components/Potolo.vue
@@ -0,0 +1,12 @@
+<script setup>
+const potolos = await  usePotolos()
+</script>
+
+<template>
+    <div class="potolo">
+        <h1>Potolo</h1>
+        <pre>
+            {{ potolos }}
+        </pre>
+    </div>
+</template>
