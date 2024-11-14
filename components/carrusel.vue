221bed544dfa3e0f47b772d88d368f8f5a7ea8a1
Andres Castillo <Andressempai03@gmail.com>
2024-11-14 00:20:45 -0500

    se creo el componente de carrusel

---
+++ components/carrusel.vue
@@ -0,0 +1,53 @@
+<script setup>
+const responsiveOptions = ref([
+  {
+    breakpoint: "1400px",
+    numVisible: 3,
+    numScroll: 1,
+  },
+  {
+    breakpoint: "1199px",
+    numVisible: 3,
+    numScroll: 1,
+  },
+  {
+    breakpoint: "767px",
+    numVisible: 2,
+    numScroll: 1,
+  },
+  {
+    breakpoint: "575px",
+    numVisible: 1,
+    numScroll: 1,
+  },
+]);
+
+const images = ref([
+  "/img/a.jpeg",
+  "/img/b.jpeg",
+  "/img/c.jpeg",
+  "/img/d.jpeg",
+]);
+</script>
+
+<template>
+  <Carousel
+    :value="images"
+    :numVisible="4"
+    :numScroll="3"
+    :responsiveOptions="responsiveOptions"
+    class="w-5/6"
+  >
+    <template #item="slotProps">
+      <div
+        class="border border-surface-200 dark:border-surface-700 rounded m-2 p-4"
+      >
+        <div class="mb-4">
+          <div class="relative mx-auto">
+            <img :src="slotProps.data" class="w-full aspect-square rounded" />
+          </div>
+        </div>
+      </div>
+    </template>
+  </Carousel>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -2,62 +2,11 @@
 definePageMeta({
   layout: "custom",
 });
-
-const responsiveOptions = ref([
-  {
-    breakpoint: "1400px",
-    numVisible: 3,
-    numScroll: 1,
-  },
-  {
-    breakpoint: "1199px",
-    numVisible: 3,
-    numScroll: 1,
-  },
-  {
-    breakpoint: "767px",
-    numVisible: 2,
-    numScroll: 1,
-  },
-  {
-    breakpoint: "575px",
-    numVisible: 1,
-    numScroll: 1,
-  },
-]);
-
-const images = ref([
-  "/img/a.jpeg",
-  "/img/b.jpeg",
-  "/img/c.jpeg",
-  "/img/d.jpeg",
-]);
 </script>
 <template>
   <div class="index">
     <div class="containerCarrusel flex justify-center">
-      <Carousel
-        :value="images"
-        :numVisible="4"
-        :numScroll="3"
-        :responsiveOptions="responsiveOptions"
-        class="w-5/6"
-      >
-        <template #item="slotProps">
-          <div
-            class="border border-surface-200 dark:border-surface-700 rounded m-2 p-4"
-          >
-            <div class="mb-4">
-              <div class="relative mx-auto">
-                <img
-                  :src="slotProps.data"
-                  class="w-full aspect-square rounded"
-                />
-              </div>
-            </div>
-          </div>
-        </template>
-      </Carousel>
+      <Carrusel />
     </div>
     <div class="flex justify-center">
       <Card class="w-5/6">
