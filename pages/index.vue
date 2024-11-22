f68dfb56b1cacf384f9522e4a8c3815be5ac20d4
nato0121 <143302259+nato0121@users.noreply.github.com>
2024-11-22 06:58:26 -0500

    Creación del componente Cajacomentario

--- pages/index.vue
+++ pages/index.vue
@@ -60,44 +60,7 @@ const images = ref([
       </Carousel>
     </div>
     <div class="flex justify-center">
-      <Card class="w-5/6">
-        <template #title>
-          <div class="flex">
-            <Avatar
-              image="/img/daggi.jpg"
-              class="mr-2"
-              shape="circle"
-              alt="User avatar"
-            />
-            <span class="font-bold">Fujoshi 69 UwU</span>
-          </div>
-        </template>
-        <template #content>
-          <Editor editorStyle="height: 140px">
-            <template v-slot:toolbar>
-              <span class="ql-formats">
-                <button v-tooltip.bottom="'Bold'" class="ql-bold"></button>
-                <button v-tooltip.bottom="'Italic'" class="ql-italic"></button>
-                <button
-                  v-tooltip.bottom="'Underline'"
-                  class="ql-underline"
-                ></button>
-              </span>
-            </template>
-          </Editor>
-        </template>
-        <template #footer>
-          <div class="flex gap-4 mt-1">
-            <Button
-              label="Cancel"
-              severity="secondary"
-              outlined
-              class="w-full"
-            />
-            <Button label="Post" class="w-full" />
-          </div>
-        </template>
-      </Card>
+     <Cajacomentario class="w-5/6"/>
     </div>
   </div>
 </template>
